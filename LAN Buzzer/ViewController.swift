3cde3aa57876e84f83b938e787d8251bece3bcb4
Student <student@raj34.istlabs.rit.edu>
2018-11-01 08:56:09 -0400

    Initial Commit

---
+++ LAN Buzzer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LAN Buzzer
+//
+//  Created by Student on 11/1/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ LAN BuzzerTests/LAN_BuzzerTests.swift
@@ -0,0 +1,33 @@
+//
+//  LAN_BuzzerTests.swift
+//  LAN BuzzerTests
+//
+//  Created by Student on 11/1/18.
+//
+
+import XCTest
+@testable import LAN_Buzzer
+
+class LAN_BuzzerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
